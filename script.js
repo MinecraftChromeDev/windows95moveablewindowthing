c52042bf2d378b8e83637c11587f3402f21aa4b6
MinecraftChromeDev <therealsamdan90@gmail.com>
2023-10-27 15:35:33 +0000

    Initial Cleanup

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-var windowElement = document.getElementById('window');
-var titleBarElement = document.getElementById('titleBar');
+var windowElement = document.getElementById("window");
+var titleBarElement = document.getElementById("titleBar");
 
 var isDragging = false;
 var dragOffset = { x: 0, y: 0 };
 
 // Make the window movable
-titleBarElement.addEventListener('mousedown', function(event) {
+titleBarElement.addEventListener("mousedown", function (event) {
   isDragging = true;
   dragOffset.x = event.clientX - windowElement.offsetLeft;
   dragOffset.y = event.clientY - windowElement.offsetTop;
 });
 
-window.addEventListener('mousemove', function(event) {
+window.addEventListener("mousemove", function (event) {
   if (isDragging) {
-    windowElement.style.left = (event.clientX - dragOffset.x) + 'px';
-    windowElement.style.top = (event.clientY - dragOffset.y) + 'px';
+    windowElement.style.left = event.clientX - dragOffset.x + "px";
+    windowElement.style.top = event.clientY - dragOffset.y + "px";
   }
 });
 
-window.addEventListener('mouseup', function() {
+window.addEventListener("mouseup", function () {
   isDragging = false;
 });
 
-
 // Make the window resizable
-var resizeHandleElements = document.getElementsByClassName('resize-handle');
+var resizeHandleElements = document.getElementsByClassName("resize-handle");
 var initialWidth = windowElement.offsetWidth;
 var initialHeight = windowElement.offsetHeight;
 
 for (var i = 0; i < resizeHandleElements.length; i++) {
-  (function() {
+  (function () {
     var resizeHandleElement = resizeHandleElements[i];
-    var direction = resizeHandleElement.getAttribute('data-direction');
+    var direction = resizeHandleElement.getAttribute("data-direction");
 
-    resizeHandleElement.addEventListener('mousedown', function(event) {
+    resizeHandleElement.addEventListener("mousedown", function (event) {
       event.stopPropagation();
       var startX = event.clientX;
       var startY = event.clientY;
@@ -43,20 +42,22 @@ for (var i = 0; i < resizeHandleElements.length; i++) {
       function resize(event) {
         var deltaX = event.clientX - startX;
         var deltaY = event.clientY - startY;
-        var newWidth = windowWidth + (direction.includes('e') ? deltaX : -deltaX);
-        var newHeight = windowHeight + (direction.includes('s') ? deltaY : -deltaY);
+        var newWidth =
+          windowWidth + (direction.includes("e") ? deltaX : -deltaX);
+        var newHeight =
+          windowHeight + (direction.includes("s") ? deltaY : -deltaY);
 
-        windowElement.style.width = newWidth + 'px';
-        windowElement.style.height = newHeight + 'px';
+        windowElement.style.width = newWidth + "px";
+        windowElement.style.height = newHeight + "px";
       }
 
       function stopResize() {
-        window.removeEventListener('mousemove', resize);
-        window.removeEventListener('mouseup', stopResize);
+        window.removeEventListener("mousemove", resize);
+        window.removeEventListener("mouseup", stopResize);
       }
 
-      window.addEventListener('mousemove', resize);
-      window.addEventListener('mouseup', stopResize);
+      window.addEventListener("mousemove", resize);
+      window.addEventListener("mouseup", stopResize);
     });
   })();
 }
